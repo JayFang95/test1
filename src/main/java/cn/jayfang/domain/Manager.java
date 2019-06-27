3f298a460b7784af7c50f91f49dd02a19225279f
jayfang <jayfang@163.com>
2019-06-27 16:16:53 +0800

    新建了一个管理类

---
+++ src/main/java/cn/jayfang/domain/Manager.java
@@ -0,0 +1,32 @@
+package cn.jayfang.domain;
+
+public class Manager {
+
+    private String Sex;
+    private String username;
+    private String role;
+
+    public String getSex() {
+        return Sex;
+    }
+
+    public void setSex(String sex) {
+        Sex = sex;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+}
